6bf42ebbe748c5a8eaa47d2fdb98eee4cc093822
Lewis <lewis.evans@uk.ibm.com>
2020-12-05 20:07:37 +0000

    Create the rainbow function & invoke it multiple times

--- merry-christmas-banner.py
+++ merry-christmas-banner.py
@@ -11,63 +11,69 @@ except ImportError:
 
 import unicornhathd
 
-print("Simple program to display Merry Christmas on a 16x16 colourful display")
 
-TEXT = 'Merry Christmas Theo! Merry Christmas Maria! Merry Christmas Owen!'
+def rainbowBanner(text):
+    FONT = ('/usr/share/fonts/truetype/freefont/FreeSansBold.ttf', 10)
 
-FONT = ('/usr/share/fonts/truetype/freefont/FreeSansBold.ttf', 10)
+    width, height = unicornhathd.get_shape()
+    unicornhathd.rotation(90)
 
-width, height = unicornhathd.get_shape()
-unicornhathd.rotation(90)
+    text_x = 1
+    text_y = 2
 
-text_x = 1
-text_y = 2
+    font_file, font_size = FONT
 
-font_file, font_size = FONT
+    font = ImageFont.truetype(font_file, font_size)
 
-font = ImageFont.truetype(font_file, font_size)
+    text_width, text_height = font.getsize(text)
 
-text_width, text_height = font.getsize(TEXT)
+    text_width += width + text_x
 
-text_width += width + text_x
+    image = Image.new('RGB', (text_width, max(height, text_height)), (0, 0, 0))
 
-image = Image.new('RGB', (text_width, max(height, text_height)), (0, 0, 0))
+    draw = ImageDraw.Draw(image)
 
-draw = ImageDraw.Draw(image)
+    draw.text((text_x, text_y), text, fill=(255, 255, 255), font=font)
 
-draw.text((text_x, text_y), TEXT, fill=(255, 255, 255), font=font)
+    for scroll in range(text_width - width):
+        for x in range(width):
 
-for scroll in range(text_width - width):
-    for x in range(width):
+            hue = (x + scroll) / float(text_width)
+            br, bg, bb = [int(n * 255)
+                          for n in colorsys.hsv_to_rgb(hue, 1.0, 1.0)]
+            for y in range(height):
+                # Get the r, g, b colour triplet from pixel x,y of our text image
+                # Our text is white on a black background, so these will all be shades of black/grey/white
+                # ie 255,255,255 or 0,0,0 or 128,128,128
+                pixel = image.getpixel((x + scroll, y))
 
-        hue = (x + scroll) / float(text_width)
-        br, bg, bb = [int(n * 255) for n in colorsys.hsv_to_rgb(hue, 1.0, 1.0)]
-        for y in range(height):
-            # Get the r, g, b colour triplet from pixel x,y of our text image
-            # Our text is white on a black background, so these will all be shades of black/grey/white
-            # ie 255,255,255 or 0,0,0 or 128,128,128
-            pixel = image.getpixel((x + scroll, y))
+                # Now we want to turn the colour of our text - shades of grey remember - into a mask for our rainbow.
+                # We do this by dividing it by 255, which converts it to the range 0.0 to 1.0
+                r, g, b = [float(n / 255.0) for n in pixel]
 
-            # Now we want to turn the colour of our text - shades of grey remember - into a mask for our rainbow.
-            # We do this by dividing it by 255, which converts it to the range 0.0 to 1.0
-            r, g, b = [float(n / 255.0) for n in pixel]
+                # We can now use our 0.0 to 1.0 range to scale our three colour values, controlling the amount
+                # of rainbow that gets blended in.
+                # 0.0 would blend no rainbow
+                # 1.0 would blend 100% rainbow
+                # and anything in between would copy the anti-aliased edges from our text
+                r = int(br * r)
+                g = int(bg * g)
+                b = int(bb * b)
 
-            # We can now use our 0.0 to 1.0 range to scale our three colour values, controlling the amount
-            # of rainbow that gets blended in.
-            # 0.0 would blend no rainbow
-            # 1.0 would blend 100% rainbow
-            # and anything in between would copy the anti-aliased edges from our text
-            r = int(br * r)
-            g = int(bg * g)
-            b = int(bb * b)
+                # Finally we colour in our finished pixel on Unicorn HAT HD
+                unicornhathd.set_pixel(width - 1 - x, y, r, g, b)
 
-            # Finally we colour in our finished pixel on Unicorn HAT HD
-            unicornhathd.set_pixel(width - 1 - x, y, r, g, b)
+        # Finally, for each step in our scroll, we show the result on Unicorn HAT HD
+        unicornhathd.show()
 
-    # Finally, for each step in our scroll, we show the result on Unicorn HAT HD
-    unicornhathd.show()
+        # And sleep for a little bit, so it doesn't scroll too quickly!
+        time.sleep(0.04)
 
-    # And sleep for a little bit, so it doesn't scroll too quickly!
-    time.sleep(0.04)
 
-unicornhathd.off()
+THEO = 'Merry Christmas Theo!'
+MARIA = 'Merry Christmas Maria!'
+OWEN = 'Merry Christmas Owen!'
+
+rainbowBanner(THEO)
+rainbowBanner(MARIA)
+rainbowBanner(OWEN)
