59cde15da7763346fd031f8d86bf0c3880f4cb13
Lewis <lewis.evans@uk.ibm.com>
2020-12-05 20:13:46 +0000

    Minor refactor and infinite looping

--- merry-christmas-banner.py
+++ merry-christmas-banner.py
@@ -11,36 +11,68 @@ except ImportError:
 
 import unicornhathd
 
+# Get the width and height of the display
+width, height = unicornhathd.get_shape()
+
+# Set the rotation of the display
+unicornhathd.rotation(90)
+
+# Select font and size
+FONT = ('/usr/share/fonts/truetype/freefont/FreeSansBold.ttf', 10)
+
 
 def rainbowBanner(text):
-    FONT = ('/usr/share/fonts/truetype/freefont/FreeSansBold.ttf', 10)
-
-    width, height = unicornhathd.get_shape()
-    unicornhathd.rotation(90)
-
+    # We want to draw our text 1 pixel in, and 2 pixels down from the top left corner
     text_x = 1
     text_y = 2
 
+    # Grab our font file and size as defined at the top of the script
     font_file, font_size = FONT
 
+    # Load the font using PIL's ImageFont
     font = ImageFont.truetype(font_file, font_size)
 
+    # Ask the loaded font how big our text will be
     text_width, text_height = font.getsize(text)
 
+    # Make sure we accommodate enough width to account for our text_x left offset
     text_width += width + text_x
 
+    # Now let's create a blank canvas wide enough to accomodate our text
     image = Image.new('RGB', (text_width, max(height, text_height)), (0, 0, 0))
 
+    # To draw on our image, we must use PIL's ImageDraw
     draw = ImageDraw.Draw(image)
 
+    # And now we can draw text at our desited (text_x, text_y) offset, using our loaded font
     draw.text((text_x, text_y), text, fill=(255, 255, 255), font=font)
 
+    # To give an appearance of scrolling text, we move a 16x16 "window" across the image we generated above
+    # The value "scroll" denotes how far this window is from the left of the image.
+    # Since the window is "width" pixels wide (16 for UHHD) and we don't want it to run off the end of the,
+    # image, we subtract "width".
     for scroll in range(text_width - width):
         for x in range(width):
 
+            # Figure out what hue value we want at this point.
+            # "x" is the position of the pixel on Unicorn HAT HD from 0 to 15
+            # "scroll" is how far offset from the left of our text image we are
+            # We want the text to be a complete cycle around the hue in the HSV colour space
+            # so we divide the pixel's position (x + scroll) by the total width of the text
+            # If this pixel were half way through the text, it would result in the number 0.5 (180 degrees)
             hue = (x + scroll) / float(text_width)
+
+            # Now we need to convert our "hue" value into r,g,b since that's what colour space our
+            # image is in, and also what Unicorn HAT HD understands.
+            # This list comprehension is just a tidy way of converting the range 0.0 to 1.0
+            # that hsv_to_rgb returns into integers in the range 0-255.
+            # hsv_to_rgb returns a tuple of (r, g, b)
             br, bg, bb = [int(n * 255)
                           for n in colorsys.hsv_to_rgb(hue, 1.0, 1.0)]
+
+            # Since our rainbow runs from left to right along the x axis, we can calculate it once
+            # for every vertical line on the display, and then re-use that value 16 times below:
+
             for y in range(height):
                 # Get the r, g, b colour triplet from pixel x,y of our text image
                 # Our text is white on a black background, so these will all be shades of black/grey/white
@@ -74,6 +106,7 @@ THEO = 'Merry Christmas Theo!'
 MARIA = 'Merry Christmas Maria!'
 OWEN = 'Merry Christmas Owen!'
 
-rainbowBanner(THEO)
-rainbowBanner(MARIA)
-rainbowBanner(OWEN)
+while True:
+    rainbowBanner(THEO)
+    rainbowBanner(MARIA)
+    rainbowBanner(OWEN)
